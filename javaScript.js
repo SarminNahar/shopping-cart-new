159ced90d2308a49230d20e590c90f4278542a6a
SarminNahar <sarmincse@gmail.com>
2021-01-29 16:14:34 +0600

    negative button

--- javaScript.js
+++ javaScript.js
@@ -22,4 +22,16 @@ document.getElementById('phone-decrease').addEventListener('click', function(){
     const phoneCount = parseInt(phoneInput.value);
     const newPhoneCount = phoneCount - 1;
     phoneInput.value = newPhoneCount;
+
+    if(newPhoneCount < 0){
+        phoneInput.value = 0;
+    }
+
+    const phoneTotal = newPhoneCount * 1219;
+    document.getElementById('phone-total').innerText = '$' + phoneTotal;
+
+    const subTotal = document.getElementById('sub-total');
+    newSubTotal = parseInt(subTotal.innerText);
+    newSubTotal = newSubTotal - 1219;
+    document.getElementById('sub-total').innerText = newSubTotal;
 })
